f0d5297009d37caac75cef4fbfd1dd049aa6ff38
Zack Brym <z.t.brym@gmail.com>
2012-11-15 18:40:18 +0000

    simulation builds well down to a termination p of 0.5. Work next to get out of infinite loop.

--- BranchArchSimulation.py
+++ BranchArchSimulation.py
@@ -3,30 +3,87 @@
 import numpy as np
 import math as m
 import csv
-import mayavi.mlab as my
+import random as r
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as p
+
+def make_xyzs(start_xyz, length, bifurcation, rotation):
+	"""This function returns a list of xyz coordinate pairs generated as a function of radial length, bifurcation angle, and rotation angle. The staring coordinates must be in the general form [[0,0],[0,0],[0,0]]."""
+	x_end = round(xyz[0][1] + length*m.sin(m.radians(bifurcation))*
+	              m.cos(m.radians(rotation)),3)
+	y_end = round(xyz[1][1] + length*m.sin(m.radians(bifurcation))*
+	              m.sin(m.radians(rotation)),3)
+	z_end = round(xyz[2][1] + length*m.cos(m.radians(bifurcation)),3)
+	return [[xyz[0][1],x_end],[xyz[1][1],y_end],[xyz[2][1],z_end]]
+
+def get_bifurcation_angles(low_value, high_value, bifurcation_set=[]):
+	"""This function returns two bifurcation angles ordered from with the larger angle first as a list of two numbers. For the simulation to work properly, the low and high values should be within the range [0-90]. A constant set of values can be input by setting bifurcation_set to a list of one or two values."""
+	if len(bifurcation_set) == 2:
+		return bifurcation_set
+	if len(bifurcation_set) == 1:
+		one = bifurcation_set[0]
+		two = r.randrange(low_value, high_value)
+		if one >= two:
+			return [one,two]
+		else:
+			return [two,one]
+	if low_value == high_value:
+		return [low_value, low_value]
+	else:
+		one = r.randrange(low_value, high_value)
+		two = r.randrange(low_value, high_value)
+		if one >= two:
+			return [one,two]
+		else:
+			return [two,one]
+
+def get_rotation_angles():
+	"""This function returns two rotation angles ordered from with the larger angle first as a list of two numbers. The second angle is exactly opposite or 180 degrees away from the first. For the simulation to work properly, the range of values has been defined as [0-180]"""
+	one = r.randrange(0,180)
+	two = -1*one
+	if one <= 90:
+		return [one,two]
+	else:
+		return [two,one]
+
+def get_termination_value(termination_probability):
+	"""This function returns a binomial response (0 or 1) in a frequency defined by the termination probability with range [0-1]. If the termination_probability is low more branches will be generated and vice versa."""
+	value = r.random()
+	if value < termination_probability:
+		return 1
+	else:
+		return 0
 
 if __name__ == "__main__":
-	termination_probability = 0.5 #range [0-1]
-	bifurcation_one = 45 #range [0-90]
-	bifurcation_two = 45 #range [0-90]
-	rotation_angle = 45 #range [0-180]
+	xyzs = [[[0, 0], [0, 0], [0, 1]]]
+	
+	termination_probability = 0.4
+	bifurcation_set = []
+	bifurcation_low = 10
+	bifurcation_high = 20
 	length = 1
-	termination_value = 0
 	diameter = 1
 
-	xyzs = [[[0,0],[0,0],[0,0],1]] #origin and trunk distance
+	for xyz in xyzs:
+		term_value = get_termination_value(termination_probability)
+		if term_value == 0:
+			bifurcation = get_bifurcation_angles(
+			                 bifurcation_low, bifurcation_high,
+			                 bifurcation_set=bifuracation_set)
+			rotation = get_rotation_angles()
+			xyzs.append(make_xyzs(xyz, length,bifurcation[0],
+			                      rotation[0]))
+			xyzs.append(make_xyzs(xyz, length,bifurcation[1],
+			                      rotation[1]))
+		
+	fig = p.figure()
+	ax = fig.gca(projection='3d')	
+	for xyz in xyzs:
+		ax.plot(xyz[0],xyz[1], xyz[2])
+	p.show()
+#for xyz in xyzs:
 
-	#for xyz in xyzs:
-	if termination_value == 0:
-		x_start = xyzs[0][0][1]
-		y_start = xyzs[0][1][1]
-		z_start = xyzs[0][2][1]
-		x_end = length*m.sin(m.radians(bifurcation_one))*m.cos(m.radians(rotation_angle))
-		y_end = length*m.sin(m.radians(bifurcation_one))*m.sin(m.radians(rotation_angle))
-		z_end = length*m.cos(m.radians(bifurcation_one))
-		xyzs.append([[x_start,x_end],[y_start,y_end],[z_start,z_end],diameter])
-        
-for xyz in xyzs:
-    my.plot3d(xyz[0],xyz[1], xyz[2],tube_radius= xyz[3]/10)
+    #my.plot3d(xyz[0],xyz[1], xyz[2],tube_radius= xyz[3]/10)
     
-my.show()
+#my.show()
