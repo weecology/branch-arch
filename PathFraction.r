70d5f58f56bcec6f357532bb75b628a62cc639bc
Zack Brym <z.t.brym@gmail.com>
2013-07-09 02:02:20 +0000

    Produce Pf for ALL spatially explicit cherry and apple!

--- PathFraction.r
+++ PathFraction.r
@@ -33,60 +33,54 @@ get_path_fraction <- function(start_twig){
 path_fractions <- matrix(ncol=7, nrow=28)
 colnames(path_fractions) <- c("species", "tree", "group", "Pf", "avg_path", "sd_path", "No_Twigs")
 
-species <- c("apple", "cherry")
 apple_trees <- c(3,4,5,13,14,15)
 
-for (i in 1:1){
-  spp <- data[data$species==species[i],]
-  if (spp$species[i] == "apple")
-      for (j in 1:6){
-        
-        tree <- spp[spp$tree==apple_trees[j],]
-        twig_start <- length(tree[tree$parent_dist==0,][,1])+1
-        no_scars <- tree[tree$length_cm!=0,]
-        no_spurs <- tree[tree$spur!='Y',]
-        just_twigs <- no_spurs[no_spurs$length_cm!=0,]
-        
-        path_fractions[(4*j-3):(4*j), 1] = species[i]
-        path_fractions[(4*j-3):(4*j), 2] = apple_trees[j]
-        total_length = 0
-        subdata<-tree
-        path_fractions[(4*j-3), 3:7] = c("All", get_path_fraction(twig_start))
-        total_length = 0
-        subdata<-no_scars
-        path_fractions[(4*j-2), 3:7] = c("No scars", get_path_fraction(twig_start))
-        total_length = 0
-        subdata<-no_spurs
-        path_fractions[(4*j-1), 3:7] = c("No spurs", get_path_fraction(twig_start))
-        total_length = 0
-        subdata<-just_twigs
-        path_fractions[(4*j), 3:7] = c("Twigs only", get_path_fraction(twig_start))
-      }
-}  
-  else
-    j=7
-    tree <- spp
-    twig_start <- length(tree[tree$parent_dist==0,][,1])+1
-    no_scars <- tree[tree$length_cm!=0,]
-    no_spurs <- tree[tree$spur!='Y',]
-    just_twigs <- no_spurs[no_spurs$length_cm!=0,]
+spp <- data[data$species=="apple",]
+for (j in 1:6){
+  tree <- spp[spp$tree==apple_trees[j],]
+  twig_start <- length(tree[tree$parent_dist==0,][,1])+1
+  no_scars <- tree[tree$length_cm!=0,]
+  no_spurs <- tree[tree$spur!='Y',]
+  just_twigs <- no_spurs[no_spurs$length_cm!=0,]
   
-    path_fractions[25:28, 1] = species[i]
-    path_fractions[25:28, 2] = 15
-    total_length = 0
-    subdata<-tree
-    path_fractions[25, 3:7] = c("All", get_path_fraction(twig_start))
-    total_length = 0
-    subdata<-no_scars
-    path_fractions[26, 3:7] = c("No scars", get_path_fraction(twig_start))
-    total_length = 0
-    subdata<-no_spurs
-    path_fractions[27, 3:7] = c("No spurs", get_path_fraction(twig_start))
-    total_length = 0
-    subdata<-just_twigs
-    path_fractions[28, 3:7] = c("Twigs only", get_path_fraction(twig_start))
-}
+  path_fractions[(4*j-3):(4*j), 1] = "apple"
+  path_fractions[(4*j-3):(4*j), 2] = apple_trees[j]
+  total_length = 0
+  subdata<-tree
+  path_fractions[(4*j-3), 3:7] = c("All", get_path_fraction(twig_start))
+  total_length = 0
+  subdata<-no_scars
+  path_fractions[(4*j-2), 3:7] = c("No scars", get_path_fraction(twig_start))
+  total_length = 0
+  subdata<-no_spurs
+  path_fractions[(4*j-1), 3:7] = c("No spurs", get_path_fraction(twig_start))
+  total_length = 0
+  subdata<-just_twigs
+  path_fractions[(4*j), 3:7] = c("Twigs only", get_path_fraction(twig_start))
+} 
 
+tree <- data[data$species=="cherry",]
+twig_start <- length(tree[tree$parent_dist==0,][,1])+1
+no_scars <- tree[tree$length_cm!=0,]
+no_spurs <- tree[tree$spur!='Y',]
+just_twigs <- no_spurs[no_spurs$length_cm!=0,]
+
+path_fractions[25:28, 1] = "cherry"
+path_fractions[25:28, 2] = 15
+total_length = 0
+subdata<-tree
+path_fractions[25, 3:7] = c("All", get_path_fraction(twig_start))
+total_length = 0
+subdata<-no_scars
+path_fractions[26, 3:7] = c("No scars", get_path_fraction(twig_start))
+total_length = 0
+subdata<-no_spurs
+path_fractions[27, 3:7] = c("No spurs", get_path_fraction(twig_start))
+total_length = 0
+subdata<-just_twigs
+path_fractions[28, 3:7] = c("Twigs only", get_path_fraction(twig_start))
+
+#write.csv(path_fractions,"PathFractions.csv")
 
 #no_broken <- data[data$broken!='y',]
 #just_whole_twigs <- just_twigs[just_twigs$broken!='y',]
