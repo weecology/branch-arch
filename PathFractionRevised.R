208d7f029e4dc867f3c24a900acd0ad13b565850
Zack Brym <z.t.brym@gmail.com>
2014-06-24 17:26:05 -0600

    resort by line to ensure parent precedes daughter in spreadsheet

--- PathFractionRevised.R
+++ PathFractionRevised.R
@@ -6,6 +6,9 @@ species <- list(list("apple",
                 list("cherry", 
                      c(1,7,10,13,15)))
 
+
+### Calculates total stem mass above branch node
+
 for (m in 1:2){
   spp <- branch[branch$species==species[[m]][1],]
   for (n in 1:length(species[[m]][[2]])){
@@ -13,12 +16,35 @@ for (m in 1:2){
     for (j in length(tree[,1]):1){
          daughters <- tree[tree$parent==tree$branch[j],]
          if (length(daughters[,1]) > 0)
-           tree$tot_stem_m[j] = tree$stem_m[j] + sum(daughters$tot_stem_m)
+           tree$tot_stem_m[j] = tree$stem_m[j] + sum(daughters$tot_stem_m, na.rm = TRUE)
          else
            tree$tot_stem_m[j] = tree$stem_m[j]     
     }
     
-    tree_path = data.frame(tree = tree$tree, branch = tree$branch, mass = tree$tot_stem_m)
+    tree_masses = data.frame(tree = tree$tree, branch = tree$branch, mass = tree$tot_stem_m)
+    
+    if (exists('paths_out'))
+      masses_out = rbind(masses_out, tree_masses)
+    else
+      masses_out <- tree_masses
+  }
+}
+
+### Calculates path length above branch node
+
+for (m in 1:2){
+  spp <- branch[branch$species==species[[m]][1],]
+  for (n in 1:length(species[[m]][[2]])){
+    tree <- spp[spp$tree==species[[m]][[2]][n],]
+    for (j in length(tree[,1]):1){
+      daughters <- tree[tree$parent==tree$branch[j],]
+      if (length(daughters[,1]) > 0)
+        tree$path_length[j] = tree$length_cm[j] + max(daughters$path_length, na.rm = TRUE)
+      else
+        tree$path_length[j] = tree$length_cm[j]     
+    }
+    
+    tree_path = data.frame(tree = tree$tree, branch = tree$branch, mass = tree$path_length)
     
     if (exists('paths_out'))
       paths_out = rbind(paths_out, tree_path)
@@ -28,6 +54,16 @@ for (m in 1:2){
 }
 
 ###Find end lengths INCOMPLETE
+
+length <- as.numeric(tree$length_cm[j])
+twig_spp <- twig[twig$species==species[[m]][1],]
+twig_tree <- twig_spp[twig_spp$tree==species[[m]][[2]][n],]
+twig_daughters <- twig_tree[twig_tree$parent==tree$branch[j],]
+end_twig <- daughters[daughters$parent_dist==length,]
+if (length(end_twig[,1])>0)
+  tree$tot_stem_m[j] = tree$length_cm[j] + max(end_twig$length_cm)
+
+
 apple_trees <- c(3,4,5,13,14,15)
 
 spp <- twig[twig$species=="apple",]
