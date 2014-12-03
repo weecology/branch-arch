0cf04042c1b73c221247596b5502590f9ec5d60c
Zack Brym <z.t.brym@gmail.com>
2014-12-03 13:40:35 -0700

    debug code for generating output data.

--- PathsAndTotsCalculation.R
+++ PathsAndTotsCalculation.R
@@ -111,12 +111,14 @@ for (m in 1:2){
       daughters <- tree[tree$parent==tree$branch[j],]
       tree$area[j] = round(2 * pi * (tree$diameter_mm[j]/2) * tree$length_cm[j],1)
       tree$volume[j] = round(pi * (tree$diameter_mm[j]/2)^2 * tree$length_cm[j],1)
-      if (length(daughters[,1]) > 0)
+      if (length(daughters[,1]) > 0){
         tree$tot_length[j] = tree$length_cm[j] + sum(daughters$tot_length, na.rm = TRUE)
         tree$tot_area[j] = tree$area[j] + sum(daughters$tot_area, na.rm = TRUE)
         tree$tot_volume[j] = tree$volume[j] + sum(daughters$tot_volume, na.rm = TRUE)
-      else
+        }
+      else{
         tree$tot_length[j] = tree$length_cm[j]
+      }
     }
     
     tree_lengths = data.frame(tree = tree$tree, branch = tree$branch, tot_length = tree$tot_length,
@@ -156,7 +158,7 @@ for (m in 1:2){
     
     tree_path_plus = data.frame(tree = tree$tree, branch = tree$branch, path_plus = tree$path_length_plus)
     
-    if (exists('paths_out'))
+    if (exists('paths_out_plus'))
       paths_out_plus = rbind(paths_out_plus, tree_path_plus)
     else
       paths_out_plus <- tree_path_plus
@@ -198,7 +200,7 @@ for (m in 1:2){
                               tot_area_plus = tree$tot_area_plus, volume_plus = tree$volume_plus,
                               tot_volume_plus = tree$tot_volume_plus)
     
-    if (exists('lengths_out'))
+    if (exists('lengths_out_plus'))
       lengths_out_plus = rbind(lengths_out_plus, tree_lengths_plus)
     else
       lengths_out_plus <- tree_lengths_plus
@@ -209,8 +211,8 @@ tots_out = data.frame(tree = branch$tree, branch = branch$branch, path = paths_o
                       area = lengths_out$area, tot_area = lengths_out$tot_area, volume = lengths_out$volume,
                       tot_volume = lengths_out$tot_volume, tot_stem_m = masses_out$mass, 
                       tot_no_twigs = no_twig_out$tot_no_twigs, tot_twig_m = twig_m_out$tot_twig_m,
-                      path_plus = tree$path_length_plus,tot_length_plus = tree$tot_length_plus, area_plus = tree$area_plus, 
-                      tot_area_plus = tree$tot_area_plus, volume_plus = tree$volume_plus,
-                      tot_volume_plus = tree$tot_volume_plus) 
+                      path_plus = paths_out_plus$path_plus, tot_length_plus = lengths_out_plus$tot_length_plus, 
+                      area_plus = lengths_out_plus$area_plus, tot_area_plus = lengths_out_plus$tot_area_plus, 
+                      volume_plus = lengths_out_plus$volume_plus, tot_volume_plus = lengths_out_plus$tot_volume_plus) 
 
 write.csv(tots_out,"PathAndTotals.csv")
