8e985c6447cda3932b21cb74703c36a5b05dd301
Zack Brym <z.t.brym@gmail.com>
2015-01-23 16:11:41 -0700

    miss saved

--- Chapter1Figs.R
+++ Chapter1Figs.R
@@ -19,6 +19,7 @@ for (i in 1:23){ #relationship columns
 ###Subtree improves R2
 
 exponent_R2 <- function(col_list, n, k, m, xlabel){
+  
   if (length(col_list) == 2){
     min_range <- c(results[[col_list[1]]][[2]][k:m], results[[col_list[2]]][[2]][k:m], flow[n], elastic[n])
     max_range <- c(results[[col_list[1]]][[3]][k:m], results[[col_list[2]]][[3]][k:m], flow[n], elastic[n])}
@@ -27,17 +28,31 @@ exponent_R2 <- function(col_list, n, k, m, xlabel){
                    results[[col_list[3]]][[2]][k:m], flow[n], elastic[n])
     max_range <- c(results[[col_list[1]]][[3]][k:m], results[[col_list[2]]][[3]][k:m], 
                    results[[col_list[3]]][[3]][k:m], flow[n], elastic[n])}
+  
   plot(range(0,1), range(min(min_range, na.rm=T), max(max_range, na.rm=T)), 
        ylab = ylabels[n], xlab=xlabel, type = 'n',
        ylim = c(min(min_range, na.rm=T), max(max_range, na.rm=T)))
-  for (i in 1:length(col_list)){
-    for(j in k:m){
-      points(results[[col_list[i]]][[4]][j], results[[col_list[i]]][[1]][j], pch = as.numeric(points[[i]][j]), 
-             bg = 'grey', cex = 2, lwd = 2.5)
-      arrows(x0=results[[col_list[i]]][[4]][j], y0=results[[col_list[i]]][[2]][j], y1=results[[col_list[i]]][[3]][j], 
-             code=3, angle=90, lwd=1.7, length=.08)
+  
+  if (length(col_list) == 3){
+    for (i in 1:length(col_list)){
+      for(j in k:m){
+        points(results[[col_list[i]]][[4]][j], results[[col_list[i]]][[1]][j], pch = as.numeric(tri_points[[i]][j]), 
+               bg = 'grey', cex = 2, lwd = 2.5)
+        arrows(x0=results[[col_list[i]]][[4]][j], y0=results[[col_list[i]]][[2]][j], y1=results[[col_list[i]]][[3]][j], 
+               code=3, angle=90, lwd=1.7, length=.08)
+      }
     }
   }
+  else{  
+    for (i in 1:length(col_list)){
+      for(j in k:m){
+        points(results[[col_list[i]]][[4]][j], results[[col_list[i]]][[1]][j], pch = as.numeric(di_points[[i]][j]), 
+               bg = 'grey', cex = 2, lwd = 2.5)
+        arrows(x0=results[[col_list[i]]][[4]][j], y0=results[[col_list[i]]][[2]][j], y1=results[[col_list[i]]][[3]][j], 
+               code=3, angle=90, lwd=1.7, length=.08)
+      }
+    }
+  }  
   abline(h = flow[n], lwd = 2, lty = 6)
   abline(h = elastic[n], lwd = 2, lty = 2)
 }
@@ -45,9 +60,12 @@ exponent_R2 <- function(col_list, n, k, m, xlabel){
 ylabels <- c("L~D", "SA~V", "D~V", "L~V", "D~SA", "L~SA", "L~M", "M~D", "M~V")
 flow <- c(2, .75, .25, .5, .33, .67, NA, NA, NA)
 elastic <- c(.67, .625, .375, .25, .6, .4, .25, 2.67, NA)
-points <- list(list(17, 24, 2, 17, 24, 2),
+tri_points <- list(list(17, 24, 2, 17, 24, 2),
                list(18, 23, 5, 18, 23, 5),
                list(15, 22, 0, 15, 22, 0))
+di_points <- list(list(17, 24, 2, 17, 24, 2),
+                   list(15, 22, 0, 15, 22, 0))
+
 
 pdf(file="ExponentR2.pdf", width= 16, height=12,family="Helvetica", pointsize=14)
 
@@ -73,7 +91,7 @@ par(mfrow= c(3,4), mar = c(4,5,1,1), cex.lab=1.5)
 exponent_R2(c(1:3), 1, 4, 6, '')
 exponent_R2(c(4:5), 2, 4, 6, '')
 exponent_R2(c(6:7), 3, 4, 6, '')
-plot(range(0,1), range(0,1), bty='n', xaxt='n', yaxt='n', xlab='', ylab='', type ='n')
+plot(range(0,1), range(0,1), bty='n', main = 'Branch Level', xaxt='n', yaxt='n', xlab='', ylab='', type ='n')
 exponent_R2(c(8:10), 4, 4, 6, '')
 exponent_R2(c(11:12), 5, 4, 6, '')
 exponent_R2(c(13:15), 6, 4, 6, '')
@@ -81,7 +99,7 @@ plot(range(0,1), range(0,1), bty='n', xaxt='n', yaxt='n', xlab='', ylab='', type
 par(xpd=T)
 legend('left', legend=c("", "", "", "", "", "", "All", "Cherry", "Apple"), 
        pch= c(17, 24, 2, 18, 23, 5, 15, 22, 0), pt.bg = 'grey', cex = 1.2, 
-       bty = 'n', title = "Segment      Path      Subtree", title.adj = -.35, ncol = 3)
+       bty = 'n', title = "Segment     Path      Subtree", title.adj = -.35, ncol = 3)
 par(xpd=F)
 exponent_R2(c(16:18), 7, 4, 6, '')
 exponent_R2(c(19:20), 8, 4, 6, 'R2')
