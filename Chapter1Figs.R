96b89150e95a10a5424b9f4440e2c650e2f6cd57
Zack Brym <z.t.brym@gmail.com>
2015-01-23 12:26:22 -0700

    generate exponent vs R2 figures for major groups" git push

--- Chapter1Figs.R
+++ Chapter1Figs.R
@@ -18,42 +18,54 @@ for (i in 1:23){ #relationship columns
 
 ###Subtree improves R2
 
-gen_exponent <- function(col_list, n){
-  plot(range(0,1), range(min(results[[n]][[2]], na.rm=T), max(results[[n]][[3]], na.rm=T)), 
-       ylab = ylabels[n], xlab="R2", xaxt = 'n', type = 'n',
-       ylim = c(min(results[[n]][[2]][4:41], na.rm=T), max(results[[n]][[3]][4:41], na.rm=T)))
-  for (j in group_list){
-    points(results[[col_list[i]]][[4]][4], results[[col_list[i]]][[1]][4], pch = 19, cex = 2)
-    arrows(x0=results[[col_list[i]]][[4]][4], y0=results[[col_list[i]]][[2]][4], y1=results[[col_list[1]]][[3]][4], 
-           code=3, angle=90, lwd=1.7, length=.08)
+exponent_R2 <- function(col_list, n, xlabel){
+  if (length(col_list == 2)){
+    min_range <- c(results[[col_list[1]]][[2]], results[[col_list[2]]][[2]])
+    max_range <- c(results[[col_list[1]]][[3]], results[[col_list[2]]][[3]])}
+  else{
+    min_range <- c(results[[col_list[1]]][[2]], results[[col_list[2]]][[2]], results[[col_list[3]]][[2]])
+    max_range <- c(results[[col_list[1]]][[3]], results[[col_list[2]]][[3]], results[[col_list[3]]][[3]])}
+  plot(range(0,1), range(min(min_range, na.rm=T), max(max_range, na.rm=T)), 
+       ylab = ylabels[n], xlab=xlabel, type = 'n',
+       ylim = c(min(min_range, na.rm=T), max(max_range, na.rm=T)))
+  for (i in 1:length(col_list)){
+    for(j in 1:6){
+      points(results[[col_list[i]]][[4]][j], results[[col_list[i]]][[1]][j], pch = as.numeric(points[[i]][j]), 
+             bg = 'grey', cex = 2, lwd = 2)
+      arrows(x0=results[[col_list[i]]][[4]][j], y0=results[[col_list[i]]][[2]][j], y1=results[[col_list[i]]][[3]][j], 
+             code=3, angle=90, lwd=1.7, length=.08)
+    }
   }
   abline(h = flow[n], lwd = 2, lty = 6)
   abline(h = elastic[n], lwd = 2, lty = 2)
 }
 
-multi_plot <- function(col_list, location){
-  par(mfrow = c(length(col_list),1), oma = c(7,0,0,0), mar = c(1,5,1,2), cex.lab = 1.5, bty = 'o')
-  gen_exponent(col_list[1])
-  par(xpd=T)
-  legend(location, legend=c("R2 = 0.99", "R2 = 0.80", "R2 = 0.67", "R2 = 0.33", "Elastic Exp", "Flow Exp"), 
-         lty = c(0,0,0,0,2,6), lwd = c(0,0,0,0,1.5,1.5), pch=19, bty = 'n', pt.cex = c(3*.99, 3*.80, 3*.67, 3*.33, 0, 0))
-  par(xpd=F)
-  for (i in col_list[-1]){ gen_exponent(i) }
-  axis(1, 1:42, sma[2:43,2], las = 2)
-}
-
 ylabels <- c("L~D", "SA~V", "D~V", "L~V", "D~SA", "L~SA", "L~M", "M~D", "M~V")
-flow <- c(2, .75, .25, .5, .33, .67, 10, 10, 10)
+flow <- c(2, .75, .25, .5, .33, .67, 10, 15, 10)
 elastic <- c(.67, .625, .375, .25, .6, .4, .25, 2.67, 10)
+points <- list(list(19, 21, 1, 17, 24, 2),
+               list(9, 7, 12, 18, 23, 5),
+               list(14, 25, 6, 15, 22, 0))
 
+pdf(file="ExponentFigures.pdf", width= 16, height=12,family="Helvetica", pointsize=14)
+par(mfrow= c(3,4), mar = c(4,5,1,1), cex.lab=1.5)
+exponent_R2(c(1:3), 1, '')
+exponent_R2(c(4:5), 2, '')
+exponent_R2(c(6:7), 3, '')
+plot(range(0,1), range(0,1), bty='n', xaxt='n', yaxt='n', xlab='', ylab='', type ='n')
+exponent_R2(c(8:10), 4, '')
+exponent_R2(c(11:12), 5, '')
+exponent_R2(c(13:15), 6, '')
+plot(range(0,1), range(0,1), bty='n', xaxt='n', yaxt='n', xlab='', ylab='', type ='n')
+par(xpd=T)
+legend('left', legend=c("", "", "", "", "", "", "", "", "", "", "", "",
+                          "Tree-", "Cherry", "Apple", 
+                          "Branch-", "Cherry", "Apple"), 
+       pch= c(19, 21, 1, 17, 24, 2, 9, 7, 12, 18, 23, 5, 14, 25, 6, 15, 22, 0), pt.bg = 'grey', cex = 1.2, 
+       bty = 'n', title = "Segment        Path        Subtree", title.adj = -.35, ncol = 3)
+par(xpd=F)
+exponent_R2(c(16:18), 7, '')
+exponent_R2(c(19:20), 8, 'R2')
+exponent_R2(c(21:22), 9, '')
+dev.off()
 
-multi_plot(c(1:3), 'bottomright')
-multi_plot(c(4:5), 'topright')
-multi_plot(c(6:7), 'topright')
-multi_plot(c(8:10), 'bottomright')
-multi_plot(c(11:12), 'bottomright')
-multi_plot(c(13:15), 'topright')
-multi_plot(c(16:18), 'topright')
-multi_plot(c(19:20), 'topright')
-multi_plot(c(21:22), 'bottomright')
-multi_plot(c(23), 'topright')
