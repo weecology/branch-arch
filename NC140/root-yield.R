7ffe97d32a116d19ab094b6627423cd02edc1b4b
Zack Brym <z.t.brym@gmail.com>
2015-05-20 14:51:08 -0500

    root-yield.R: complete duncans

--- NC140/root-yield.R
+++ NC140/root-yield.R
@@ -1,6 +1,7 @@
 ### This script analysis the NC-140 Rootstock roots biomass.
 
 library('dplyr')
+library('agricolae')
 
 roots_mass   <- mutate(read.csv('RootDryMass.csv', head = T, sep = ','),
                        location = row + num / 100,
@@ -31,10 +32,20 @@ rootstocks   <- c('Bud.9', 'CG.6210', 'M.9', 'CG.3041', 'CG.3041',
                   'CG.6210', 'CG.5935', 'CG.5935', 'M.9', 'CG.3041', 
                   'Bud.9', 'CG.6210', 'M.9', 'Bud.9', 'M.9')
 
-tree_totals  <- mutate(tree_tots, 
+tree_totals  <- mutate(tree_tots,
+                       location = factor(location),
                        stump_mass = stump_mass$stump_wgt_kg * 1000,
                        id = ids, 
                        rootstock = factor(rootstocks))
+
+roots_list <- c()
+for (r in rootstocks){
+  roots_list <- append(roots_list, rep(r, 45))
+}
+
+roots_mass <- mutate(roots_mass,
+                     location = factor(location),
+                     rootstock = factor(roots_list))
 
 # Group by rootstock ----
 by_rootstock <- group_by(tree_totals, rootstock)
@@ -55,12 +66,72 @@ roots_totals <- arrange(
 # Treatment is fixed effect NOT random
 # error is biggest scale to smallest
 
-model <- aov(total_roots ~ rootstock*location+Error(rootstock/location), data = tree_totals)
-model <- aov(avg_total ~ rootstock, data = roots_totals)
+# Rootstock
+duncan_rootstock <- c()
+summary_rootstock <- c()
+
+model <- aov(stump_mass ~ rootstock, data = tree_totals)
+duncan_rootstock$stump <- duncan.test(model, "rootstock")$groups
+summary_rootstock$stump <- summary(model)
 
 model <- aov(total_roots ~ rootstock, data = tree_totals)
-comparison <- duncan.test(model,"rootstock")
-comparison$groups
+duncan_rootstock$total_roots <- duncan.test(model, "rootstock")$groups
+summary_rootstock$total_roots <- summary(model)
+
+model <- aov(total_large ~ rootstock, data = tree_totals)
+duncan_rootstock$total_large <- duncan.test(model, "rootstock")$groups
+summary_rootstock$total_large <- summary(model)
+
+model <- aov(total_medium ~ rootstock, data = tree_totals)
+duncan_rootstock$total_medium <- duncan.test(model, "rootstock")$groups
+summary_rootstock$total_medium <- summary(model)
+
+model <- aov(total_small ~ rootstock, data = tree_totals)
+duncan_rootstock$total_small <- duncan.test(model, "rootstock")$groups
+summary_rootstock$total_small <- summary(model)
+
+# Location by Rootstock
+
+duncan_location <- c()
+summary_location <- c()
+
+model <- aov(total_m ~ rootstock*location, data = roots_mass)
+# model <- aov(total_m ~ rootstock*location+Error(rootstock/location), data = roots_mass) GIVES ERROR
+duncan_location$total_roots <- duncan.test(model, "location")$groups
+summary_location$total_roots <- summary(model)
+
+model <- aov(large ~ rootstock*location, data = roots_mass)
+duncan_location$large <- duncan.test(model, "location")$groups
+summary_location$large <- summary(model)
+
+model <- aov(medium ~ rootstock*location, data = roots_mass)
+duncan_location$medium <- duncan.test(model, "location")$groups
+summary_location$medium <- summary(model)
+
+model <- aov(small ~ rootstock*location, data = roots_mass)
+duncan_location$small <- duncan.test(model, "location")$groups
+summary_location$small <- summary(model)
+
+# Depth by Location by Roostock
+
+duncan_depth <- c()
+summary_depth <- c()
+
+model <- aov(total_m ~ rootstock*location*max_depth, data = roots_mass)
+duncan_depth$total_roots <- duncan.test(model, "max_depth")$groups
+summary_depth$total_roots <- summary(model)
+
+model <- aov(large ~ rootstock*location*max_depth, data = roots_mass)
+duncan_depth$large <- duncan.test(model, "max_depth")$groups
+summary_depth$large <- summary(model)
+
+model <- aov(medium ~ rootstock*location*max_depth, data = roots_mass)
+duncan_depth$medium <- duncan.test(model, "max_depth")$groups
+summary_depth$medium <- summary(model)
+
+model <- aov(small ~ rootstock*location*max_depth, data = roots_mass)
+duncan_depth$small <- duncan.test(model, "max_depth")$groups
+summary_depth$small <- summary(model)
 
 # Remnant code ----
 
