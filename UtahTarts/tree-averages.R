592aecef2322fe795cb220654206fdf702e500cd
Zack Brym <z.t.brym@gmail.com>
2015-09-24 16:33:33 -0500

    tree-averages.R: generate output for blocks without light

--- UtahTarts/tree-averages.R
+++ UtahTarts/tree-averages.R
@@ -1,16 +1,5 @@
 ###This script generates the per tree averages of light and sugar content (brix) in the Utah Co tarts data.
 
-gen_plot <- function(x, y, x_lab, y_lab, r2) {
-  test <- lm(y~x)
-  plot(x, y,
-       xlim=c(min(x, na.rm=T)-0.2, max(x, na.rm=T)+0.2), 
-       ylim=c(min(y, na.rm=T)-0.2, max(y, na.rm=T)+0.2),
-       xlab=x_lab, ylab=y_lab, cex.lab=1.5, cex=2.5, pch=19, col="black")
-  abline(test$coefficients[1], test$coefficients[2], lwd=3, lty=2)
-  legend('topright', legend=r2, bty='n', cex=3)
-  print(summary(test)$r.squared)
-}
-
 scaffold <- read.csv('scaffold.csv', sep=',', head=T)
 light_raw <- read.csv('light.csv', sep=',', head=T)
 sugar <- read.csv('sugars.csv', sep=',', head=T)
@@ -27,7 +16,7 @@ for (i in 1:15){
   sugar_block <- sugar[sugar$id==i,]
   scaffold_block <- subset(scaffold, light_id==i)
   
-  subout <- matrix(ncol= 12, nrow = 5)
+  subout <- matrix(ncol= 16, nrow = 5)
   
   for (j in 1:5){
     
@@ -35,20 +24,25 @@ for (i in 1:15){
     sugar_ind <- sugar_block[sugar_block$tree==j,]
     scaffold_id <- scaffold_block[scaffold_block$tree==j,]
     trunk <- scaffold_id[scaffold_id$scaffold==0,]
+    scaffolds <- scaffold_id[scaffold_id$scaffold!=0,]
     light_pos <- light_ind[light_ind$absorbed > 0,]
     
-    subout[j,1] = i
-    subout[j,2] = j
-    subout[j,3] = trunk$diameter
-    subout[j,4] = round(mean(scaffold_id$length), 0)
-    subout[j,5] = round(mean(scaffold_id$diameter), 0)
-    subout[j,6] = round(mean(light_pos$absorbed), 3)
-    subout[j,7] = round(mean(light_pos$extinction), 3)
-    subout[j,8] = round(mean(light_ind$extinction[1:8]), 3)
-    subout[j,9] = round(mean(light_ind$extinction[9:16]), 3)
-    subout[j,10] = round(mean(sugar_ind$sugar, na.rm=T), 2)
-    subout[j,11] = round(mean(sugar_ind$sugar[1:4], na.rm=T), 2)
-    subout[j,12] = round((subout[j,11] - sugar_ind$sugar[5]) / subout[j,11],3)
+    subout[j, 1] = i
+    subout[j, 2] = j
+    subout[j, 3] = trunk$diameter
+    subout[j, 4] = round(pi*(trunk$diameter/20)^2, 0)
+    subout[j, 5] = dim(scaffolds)[1]
+    subout[j, 6] = round(mean(scaffolds$length), 0)
+    subout[j, 7] = round(sd(scaffolds$length), 3)
+    subout[j, 8] = round(mean(scaffolds$diameter), 0)
+    subout[j, 9] = round(sd(scaffolds$diameter), 3)
+    subout[j, 10] = round(mean(light_pos$absorbed), 3)
+    subout[j, 11] = round(mean(light_pos$extinction), 3)
+    subout[j, 12] = round(mean(light_ind$extinction[1:8]), 3)
+    subout[j, 13] = round(mean(light_ind$extinction[9:16]), 3)
+    subout[j, 14] = round(mean(sugar_ind$sugar, na.rm=T), 2)
+    subout[j, 15] = round(mean(sugar_ind$sugar[1:4], na.rm=T), 2)
+    subout[j, 16] = round((subout[j,15] - sugar_ind$sugar[5]) / subout[j,15],3)
   }
   
   if (exists('averages'))
@@ -56,66 +50,48 @@ for (i in 1:15){
   
   else
     averages <- subout
-    colnames(averages) = c('block', 'tree', 'trunk', 'avg_scaffold_l', 
-                           'avg_scaffold_d', 'avg_absorbed', 'avg_extinction', 
-                           'avg_extinction_low', 'avg_extinction_high', 
-                           'avg_sugar', 'avg_sugar_out', 'sugar_diff') 
+    colnames(averages) = c('block', 'tree', 'trunk_mm', 'TCSA_cm2', 'no_scaffolds', 
+                           'avg_scaffold_l', 'sd_scaffold_l',
+                           'avg_scaffold_d', 'sd_scaffold_d', 'avg_absorbed', 
+                           'avg_extinction', 'avg_extinction_low', 
+                           'avg_extinction_high', 'avg_sugar', 'avg_sugar_out', 
+                           'sugar_diff') 
 }
 
-#write.csv(averages, "tree-averages.csv")
+#write.csv(averages, "tree-averages-light.csv")
 
+for (i in 1:38){
+  
+  scaffold_block <- subset(scaffold, id==i)
+  
+  subout <- matrix(ncol= 9, nrow = 5)
+  
+  for (j in 1:5){
+    
+    scaffold_id <- scaffold_block[scaffold_block$tree==j,]
+    trunk <- scaffold_id[scaffold_id$scaffold==0,]
+    scaffolds <- scaffold_id[scaffold_id$scaffold!=0,]
+    
+    subout[j, 1] = i
+    subout[j, 2] = j
+    subout[j, 3] = trunk$diameter
+    subout[j, 4] = round(pi*(trunk$diameter/20)^2, 0)
+    subout[j, 5] = dim(scaffolds)[1]
+    subout[j, 6] = round(mean(scaffolds$length), 0)
+    subout[j, 7] = round(sd(scaffolds$length), 3)
+    subout[j, 8] = round(mean(scaffolds$diameter), 0)
+    subout[j, 9] = round(sd(scaffolds$diameter), 3)
 
-### Tree average regression
+  }
+  
+  if (exists('averages_all'))
+    averages_all <- rbind(averages_all, subout)
+  
+  else
+    averages_all <- subout
+    colnames(averages_all) = c('block', 'tree', 'trunk_mm', 'TCSA_cm2', 'no_scaffolds', 
+                               'avg_scaffold_l', 'sd_scaffold_l',
+                               'avg_scaffold_d', 'sd_scaffold_d') 
+}
 
-averages <- read.csv("tree-averages.csv")
-TCSA <- pi*(averages$trunk/20)^2
-
-#trunk diameter vs. scaffold diameter
-gen_plot(averages$trunk, averages$avg_scaffold_d,
-         'Trunk Diameter [mm]', 'Avg. Scaffold Diameter',
-         expression(R^2 == 0.898))
-
-#trunk diameter vs. scaffold length
-gen_plot(averages$trunk, averages$avg_scaffold_l,
-         'Trunk Diameter [mm]', 'Avg. Scaffold Length',
-         expression(R^2 == 0.872))
-
-#scaffold diameter vs. scaffold length
-gen_plot(averages$avg_scaffold_d, averages$avg_scaffold_l,
-         'Avg Scaffold Diameter', 'Avg. Scaffold Length',
-         expression(R^2 == 0.811))
-
-#TCSA vs. scaffold diameter
-gen_plot(TCSA, averages$avg_scaffold_d,
-         'TCSA [cm2]', 'Avg. Scaffold Diameter',
-         expression(R^2 == 0.876))
-
-#TCSA vs. scaffold length
-gen_plot(TCSA, averages$avg_scaffold_l,
-         'TCSA [cm2]', 'Avg. Scaffold Length',
-         expression(R^2 == 0.839))
-
-#trunk diameter vs. sugar (R2 = 0.438)
-gen_plot(averages$trunk, averages$avg_sugar, 
-         'Trunk Diameter [mm]', 'Fruit Sugar Content[Brix]',
-         expression(R^2 == 0.438))
-
-#TCSA vs. sugar (R2 = 0.365)
-gen_plot(TCSA, averages$avg_sugar, 
-         'TCSA [cm2]', 'Fruit Sugar Content[Brix]',
-         expression(R^2 == 0.365))  
-
-#scaffold length vs. sugar 
-gen_plot(averages$avg_scaffold_l, averages$avg_sugar,
-         'Average Scaffold Length [cm]', 'Fruit Sugar Content [Brix]', 
-         expression(R^2 == 0.554))  
-
-#scaffold diameter vs. sugar
-gen_plot(averages$avg_scaffold_d, averages$avg_sugar, 
-         'Average Scaffold Diameter [mm]', 'Fruit Sugar Content [Brix]', 
-         expression(R^2 == 0.381))  
-
-#light abs vs. sugar
-gen_plot(averages$avg_absorbed, averages$avg_sugar, 
-         'Average Light Absorption', 'Fruit Sugar Content [Brix]',
-         expression(R^2 == 0.097))  
+#write.csv(averages_all, "tree-averages-all.csv")
