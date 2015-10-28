9c0f7fef6275f9525bd50a13a0359b3937e359ea
Zack Brym <z.t.brym@gmail.com>
2015-10-28 17:03:36 -0500

    std-yield.R: add harvest index

--- NC140/std-yield.R
+++ NC140/std-yield.R
@@ -1,16 +1,31 @@
-library('dplyr')
+### This code generates and analyses standard yield metric in hort research.
+
+library(dplyr)
+library(agricolae)
+library(ggplot2)
 
 tree_sum <- read.csv("TreeSummary.csv")
 yield <- read.csv("AppleYield.csv", sep =',', head=T)
 tree_yield <- inner_join(tree_sum, yield)
-TCSA <- pi*(tree_yield$trunk_diam_cm/2)^2  # Also as ...$trunk_diam_cm/2
+TCSA <- pi*(tree_yield$trunk_diam_cm/2)^2
 
-model <- aov(TCSA ~ as.factor(rootstocks))
+model <- aov(TCSA ~ as.factor(tree_yield$rootstock))
 summary_TCSA <- summary(model)
-test  <- duncan.test(model, "as.factor(rootstocks)")
+test  <- duncan.test(model, "as.factor(tree_yield$rootstock)")
 duncan_TCSA  <- test$groups
 
-model <- aov((tree_yield$cum_yield/TCSA) ~ as.factor(rootstocks))
+model <- aov((tree_yield$cum_yield/TCSA) ~ as.factor(tree_yield$rootstock))
 summary_YE <- summary(model)
-test  <- duncan.test(model, "as.factor(rootstocks)")
+test  <- duncan.test(model, "as.factor(tree_yield$rootstock)")
 duncan_YE  <- test$groups
+
+model <- aov(cum_yield / (tot_stem_m + tot_twig_m)  ~ as.factor(rootstock),
+             data = tree_yield)
+summary_HI <- summary(model)
+test  <- duncan.test(model, "as.factor(rootstock)")
+duncan_HI  <- test$groups
+
+ggplot(tree_yield) +
+  geom_point(aes(x = (tot_stem_m + tot_twig_m), y = cum_yield, 
+                 color = factor(rootstock), size = 5))
+
