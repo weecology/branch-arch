878ffc1540fcf7b085f092dd30299c7977d783ac
Zack Brym <z.t.brym@gmail.com>
2015-11-11 17:02:41 -0600

    generate hort-allom and std-yield analysis for Ch2 rewrite

--- NC140/hort-allom.R
+++ NC140/hort-allom.R
@@ -6,22 +6,77 @@ library(ggplot2)
 tree_sum <- read.csv("TreeSummary.csv")
 yield <- read.csv("AppleYield.csv", sep =',', head=T)
 tree_yield <- inner_join(tree_sum, yield)
-TCSA <- pi*(tree_yield$trunk_diam_cm/2)^2
+tree_yield <- dplyr::mutate(tree_yield, TCSA = pi*(trunk_diam_cm/2)^2)
 sma <- read.csv("SMAResults.csv")
 
-test <- sma(log10((tree_yield$tot_stem_m + tree_yield$tot_twig_m)) ~ log10(TCSA))
-test <- sma(log10((tree_yield$height)) ~ log10(TCSA))
-test <- sma(log10((tree_yield$tot_stem_m + tree_yield$tot_twig_m)) ~ log10(tree_yield$height))
-test <- sma(log10((tree_yield$tot_stem_m + tree_yield$tot_twig_m)) ~ log10(tree_yield$tot_volume))
-test <- sma(log10((tree_yield$tot_volume)) ~ log10(TCSA))
+### Individual Level
+
+test <- sma(log10((tot_stem_m + tot_twig_m)) ~ log10(TCSA), data = tree_yield)
+test <- sma(log10(height) ~ log10(TCSA), data = tree_yield)
+test <- sma(log10((tot_stem_m + tot_twig_m)) ~ log10(height), data = tree_yield)
+test <- sma(log10((tot_stem_m + tot_twig_m)) ~ log10(tot_volume), data = tree_yield)
+test <- sma(log10((tot_volume)) ~ log10(TCSA), data = tree_yield)
+test <- sma(log10(cum_yield) ~ log10(TCSA), data = tree_yield)
+test <- sma(log10(tot_stem_m + tot_twig_m) ~ log10(pi*(canopy_spread/2)^2), data = tree_yield)
+test <- sma(log10(cum_yield) ~ log10(pi*(canopy_spread/2)^2), data = tree_yield)
 summary(test)
 
+mass_allometry <- c()
+cum_yield_allometry <- c()
 for (a in tree_sum[c(-1,-2)]) {
-  ifelse(length(a[a<=0]) > 0, 
-         print('No test'),
-         print(sma(log10((tree_yield$tot_stem_m + tree_yield$tot_twig_m)) ~ 
-                     log10(a))$r2[[1]]))
+  mass_allometry <- c(mass_allometry, 
+                      ifelse(length(a[a<=0]) > 0, 'No test',
+                        sma(log10((tree_yield$tot_stem_m + tree_yield$tot_twig_m)) ~ 
+                        log10(a))$r2[[1]]))
+  cum_yield_allometry <- c(cum_yield_allometry, 
+                      ifelse(length(a[a<=0]) > 0, 'No test',
+                             sma(log10(tree_yield$cum_yield) ~ 
+                                   log10(a))$r2[[1]]))
 }
+allometry <- cbind(names(tree_sum[c(-1,-2)]), 
+                   mass_allometry, cum_yield_allometry)
+
+
+### Rootstock Level
+
+tree_yield_roots <- dplyr::arrange(dplyr::summarize(
+                              dplyr::group_by(tree_yield, rootstock),
+                              avg_TCSA = round(mean(TCSA), 3),
+                              avg_height = round(mean(height), 3),
+                              avg_max_path = round(mean(max_path), 3),
+                              avg_stem_length = round(mean(tot_length), 3),
+                              avg_stem_area = round(mean(tot_area), 3),
+                              avg_stem_volume = round(mean(tot_volume), 3),
+                              avg_stem_mass = round(mean(tot_stem_m + tot_twig_m), 3),
+                              avg_canopy_spread = round(mean(canopy_spread),3),
+                              avg_canopy_area = round(mean(pi*(canopy_spread/2)^2), 3),
+                              avg_canopy_volume = round(mean(canopy_volume), 3),
+                              avg_cum_yield = round(mean(cum_yield), 3)),
+                            avg_TCSA)
+
+mass_allom_roots <- c()
+cum_yield_allom_roots <- c()
+for (a in tree_yield_roots[-1]) {
+  mass_allom_roots <- c(mass_allom_roots, 
+                        ifelse(length(a[a<=0]) > 0, 'No test',
+                               sma(log10(tree_yield_roots$avg_stem_mass) ~ 
+                                   log10(a))$r2[[1]]))
+  cum_yield_allom_roots <- c(cum_yield_allom_roots, 
+                             ifelse(length(a[a<=0]) > 0, 'No test',
+                                    sma(log10(tree_yield_roots$avg_cum_yield) ~ 
+                                        log10(a))$r2[[1]]))
+}
+allom_roots <- cbind(names(tree_yield_roots[-1]), 
+                   mass_allom_roots, cum_yield_allom_roots)
+
+
+### Root Data
+
+tree_root <- left_join(tree_sum, read.csv('StumpMass.csv'))
+test <- sma(log10((tot_stem_m + tot_twig_m + stump_wgt_kg)) ~ log10(TCSA), data = tree_root)
+test <- sma(log10((tot_stem_m + tot_twig_m + stump_wgt_kg)) ~ log10(cum_yield), data = tree_root)
+
+### Visualize
 
 ggplot() +
   geom_point(aes(x=log10(TCSA), 
--- NC140/std-yield.R
+++ NC140/std-yield.R
@@ -11,7 +11,7 @@ library(dplyr)
 tree_sum <- read.csv("TreeSummary.csv")
 yield <- read.csv("AppleYield.csv", sep =',', head=T)
 tree_yield <- inner_join(tree_sum, yield)
-TCSA <- pi*(tree_yield$trunk_diam_cm/2)^2
+tree_yield <- dplyr::mutate(tree_yield, TCSA = pi*(trunk_diam_cm/2)^2)
 sma <- read.csv("SMAResults.csv")
 
 ## ANOVA and Duncan's test
@@ -33,7 +33,7 @@ summary_cum_yield <- summary(model)
 test  <- duncan.test(model, "as.factor(rootstock)")
 duncan_cum_yield  <- test$groups
 
-model <- aov((tree_yield$yield/TCSA) ~ as.factor(tree_yield$rootstock))
+model <- aov((tree_yield$yield/tree_yield$TCSA) ~ as.factor(tree_yield$rootstock))
 summary_ye <- summary(model)
 test  <- duncan.test(model, "as.factor(tree_yield$rootstock)")
 duncan_ye  <- test$groups
@@ -42,6 +42,21 @@ model <- aov((tree_yield$cum_yield/TCSA) ~ as.factor(tree_yield$rootstock))
 summary_YE <- summary(model)
 test  <- duncan.test(model, "as.factor(tree_yield$rootstock)")
 duncan_YE  <- test$groups
+
+model <- aov((cum_yield/canopy_spread) ~ rootstock, tree_yield)
+summary_YS <- summary(model)
+test  <- duncan.test(model, "rootstock")
+duncan_YS  <- test$groups
+
+model <- aov((pi*(canopy_spread/2)^2) ~ rootstock, tree_yield)
+summary_canopy_area <- summary(model)
+test  <- duncan.test(model, "rootstock")
+duncan_canopy_area  <- test$groups
+
+model <- aov((cum_yield/(pi*(canopy_spread/2)^2)) ~ rootstock, tree_yield)
+summary_YA <- summary(model)
+test  <- duncan.test(model, "rootstock")
+duncan_YA  <- test$groups
 
 model <- aov(1000*yield / (tot_stem_m + tot_twig_m + 1000*yield)  ~ 
                as.factor(rootstock), data = tree_yield)
@@ -58,21 +73,21 @@ duncan_HI  <- test$groups
 ## Allometry
 
 Y0 <- -0.86  # str_split(sma$X.Subtree..7[3], ";")[[1]][1]
-a <- 2.5  # str_split(sma$X.Subtree..7[3], ";")[[1]][4]
+a <- 2.67  # str_split(sma$X.Subtree..7[3], ";")[[1]][4]
 mass <- 10^(as.numeric(Y0) +  as.numeric(a)*log10(tree_yield$trunk_diam))
 
 allom_mass <- lm(mass ~ (tree_yield$tot_stem_m + tree_yield$tot_twig_m))  #0.972
 
-model <- aov(1000*yield / mass  ~ as.factor(rootstock),
-             data = tree_yield)
+model <- aov(1000*tree_yield$yield / (1000*tree_yield$yield + mass)  ~ 
+               as.factor(tree_yield$rootstock))
 summary_YI_a <- summary(model)
-test  <- duncan.test(model, "as.factor(rootstock)")
+test  <- duncan.test(model, "as.factor(tree_yield$rootstock)")
 duncan_YI_a  <- test$groups
 
-model <- aov(1000*cum_yield / mass  ~ as.factor(rootstock),
-             data = tree_yield)
+model <- aov(1000*tree_yield$cum_yield / (1000*tree_yield$cum_yield + mass)  ~ 
+               as.factor(tree_yield$rootstock))
 summary_HI_a <- summary(model)
-test  <- duncan.test(model, "as.factor(rootstock)")
+test  <- duncan.test(model, "as.factor(tree_yield$rootstock)")
 duncan_HI_a  <- test$groups
 
 ## Test SMA for TCSA
@@ -81,6 +96,23 @@ test <- sma(log10((tree_yield$tot_stem_m + tree_yield$tot_twig_m)) ~
               log10(tree_yield$trunk_diam))
 
 test <- sma((tree_yield$tot_stem_m + tree_yield$tot_twig_m) ~ TCSA)
+
+
+### YE correlation
+
+morph_yield_eff <- c()
+for (morph in tree_yield[c(-1,-2)]) {
+  test <- lm(tree_yield$cum_yield / tree_yield$TCSA ~ morph)
+  morph_yield_eff <- append(morph_yield_eff, round(summary.lm(test)$r.squared, 3))
+}
+morph_yield_eff <- cbind(names(tree_yield[c(-1,-2)]), morph_yield_eff)
+
+morph_harvest_index <- c()
+for (morph in tree_yield[c(-1,-2)]) {
+  test <- lm(tree_yield$cum_yield / (tree_yield$tot_stem_m + tree_yield$tot_twig_m) ~ morph)
+  morph_harvest_index <- append(morph_harvest_index, round(summary.lm(test)$r.squared, 3))
+}
+morph_harvest_index <- cbind(names(tree_yield[c(-1,-2)]), morph_harvest_index)
 
 ## Visualize
 
