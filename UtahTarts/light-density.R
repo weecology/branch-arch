081eca03d14a509aafb79fde16de10d8ba81c650
Zack Brym <z.t.brym@gmail.com>
2016-03-05 18:18:55 -0600

    light-density.R: minor fig edits

--- UtahTarts/light-density.R
+++ UtahTarts/light-density.R
@@ -17,11 +17,15 @@ avg_l_block_code <- inner_join(avg_light, block_code, by = c('block' = 'light_id
 avg_vol_light_tree <- inner_join(avg_l_block_code, tree_volumes, 
                                  by = c('block_code' = 'block', 'tree'))
 
-png("light.png", width = 600, height = 1200)
+grower_conv <- data.frame(grower=levels(avg_vol_light_tree$grower), 
+                          grower.x=as.factor(seq_along(levels(avg_vol_light_tree$grower))))
+avg_vol_light_tree <- left_join(avg_vol_light_tree, grower_conv)
+
+png("light.png", width = 1200, height = 900)
 A1 <- ggplot(avg_vol_light_tree, aes(x=(1-low_light), y=avg_sugar_out)) +
   geom_smooth(method = "lm", formula = y ~ x,
               fill='white', color = 'black', size = 2) +
-  geom_point(aes(shape=grower), size=10, bg="black") +
+  geom_point(aes(shape=grower.x), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
   annotate("text", x=0.24, y=8.2, size=18, 
            label=lm_eqn(avg_sugar_out~low_light, df=avg_vol_light_tree), parse = TRUE) +
@@ -32,7 +36,7 @@ A1 <- ggplot(avg_vol_light_tree, aes(x=(1-low_light), y=avg_sugar_out)) +
 A2 <- ggplot(avg_vol_light_tree, aes(x=height, y=(1-low_light))) +
   geom_smooth(method = "lm", formula = y ~ x,
               fill='white', color = 'black', size = 2) +
-  geom_point(aes(shape=grower), size=10, bg="black") +
+  geom_point(aes(shape=grower.x), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
   scale_x_continuous(limits=c(300,600)) +
   annotate("text", x=400, y=0.14, size=18, 
@@ -44,7 +48,7 @@ A2 <- ggplot(avg_vol_light_tree, aes(x=height, y=(1-low_light))) +
 A3 <- ggplot(avg_vol_light_tree, aes(x=spread, y=(1-low_light))) +
   geom_smooth(method = "lm", formula = y ~ x,
               fill='white', color = 'black', size = 2) +
-  geom_point(aes(shape=grower), size=10, bg="black") +
+  geom_point(aes(shape=grower.x), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
   scale_x_continuous(limits=c(200,600)) +
   annotate("text", x=310, y=0.14, size=18, 
@@ -52,7 +56,19 @@ A3 <- ggplot(avg_vol_light_tree, aes(x=spread, y=(1-low_light))) +
   labs(x="Canopy Spread [cm]", y="Light [% full sun]", 
        shape = "", title = "C") +
   theme_classic(base_size = 24, base_family = "Helvetica") +
+  theme(axis.title=element_text(size=36), legend.position="none")
+A4 <- ggplot(avg_vol_light_tree, aes(x=frustum, y=(1-low_light))) +
+  geom_smooth(method = "lm", formula = y ~ x,
+              fill='white', color = 'black', size = 2) +
+  geom_point(aes(shape=grower.x), size=10, bg="black") +
+  scale_shape_manual(values=c(21:25)) +
+  scale_x_continuous(limits=c(0,80)) +
+  annotate("text", x=60, y=0.14, size=18, 
+           label=lm_eqn(low_light~frustum, df=avg_vol_light_tree), parse = TRUE) +
+  labs(x="Volume [m3]", y="Light [% full sun]", 
+       shape = "", title = "D") +
+  theme_classic(base_size = 24, base_family = "Helvetica") +
   theme(axis.title=element_text(size=36), legend.position=c(0.7,0.9),
         legend.direction = "horizontal")
-multiplot(A1, A2, A3, cols=1)
+multiplot(A1, A3, A2, A4, cols=2)
 dev.off()
