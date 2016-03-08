51e22935c3933ba7f0e2343982ecd09aef0e748b
Zack Brym <z.t.brym@gmail.com>
2016-03-08 10:31:49 -0600

    branch-angle.R: add all branch calc; add sd

--- NC140/branch-angle.R
+++ NC140/branch-angle.R
@@ -6,14 +6,18 @@ data <- read.csv("BranchSegments.csv")
 apple <- filter(data, species == "apple")
 explicit <- filter(apple, declination != "NA")
 by_rootstock <- group_by(explicit, tree)
+all <- summarize(by_rootstock, avg_all = round(90-mean(declination), 1), 
+                 sd_all = round(sd(declination), 0))
 zeros <- filter(by_rootstock, declination <= 0)
 ninties <- filter(by_rootstock, declination == 90)
 middles <- filter(filter(by_rootstock, declination > 0), declination < 90)
 zeros_n <- summarize(zeros, zeros_count = n())
 ninties_n <- summarize(ninties, ninties_count = n())
-middles_n <- summarize(middles, mid_count = n(), avg_angle = mean(declination))
-angles <- left_join(left_join(zeros_n, ninties_n), middles_n)
-angles <- mutate(angles, z_n_ratio = zeros_count / ninties_count)
+middles_n <- summarize(middles, mid_count = n(), 
+                       avg_angle = round(90-mean(declination), 1), 
+                       sd_angle = round(sd(declination), 0))
+angles <- left_join(left_join(left_join(zeros_n, ninties_n), middles_n), all)
+angles <- mutate(angles, z_n_ratio = round(zeros_count / ninties_count, 2))
 
 cum_yield <- read.csv("AppleYield.csv")
 angles_yield <- left_join(angles, cum_yield)
