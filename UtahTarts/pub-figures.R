8c56b01286897787f9a5789aba1630c61d8ed646
brymz <z.t.brym@gmail.com>
2017-02-07 17:05:12 -0500

    pub-figures.R: add grower-age-class-pub

--- UtahTarts/pub-figures.R
+++ UtahTarts/pub-figures.R
@@ -12,51 +12,47 @@ age_zero <- filter(avg_vol, !is.na(age))
 # Fig 1
 png("grower-age-class-pub.png", width = 1200, height = 900)
 Z1 <- ggplot(age_zero, aes(x=age, y=TCSA, group=age_class)) +
-  geom_smooth(method = "lm", fill='grey', color='black', size = 2) +
+  geom_smooth(method="lm", alpha=0, color='black', size=2, fullrange=T) +
   geom_point(aes(shape=grower), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
-  annotate("text", x=17, y=100, size=10, 
-           label=lm_eqn(TCSA~age, df=young, params=T), parse = T) +
-  annotate("text", x=23, y=200, size=10, 
-           label=lm_eqn(TCSA~age, df=old, params=T), parse = T) +
-  labs(x="Age", y="TCSA [cm2]", 
-       shape = "", title = "A") +
-  theme_classic(base_size = 24, base_family = "Helvetica") +
+  ylim(NA, max(age_zero$TCSA)) +
+  geom_segment(aes(x=16, y=175, xend=14.5, yend=240), size=1.25,
+               arrow=arrow(length=unit(0.08, "npc"))) +
+  annotate("text", x=16, y=150, size=10, label=lm_intercept(TCSA~age)) +
+  labs(x="Age", y="TCSA [cm2]", shape = "", title = "A") +
+  theme_classic(base_size=24, base_family="Helvetica") +
   theme(axis.title=element_text(size=36), legend.position="none")
 Z2 <- ggplot(age_zero, aes(x=age, y=spread, group=age_class)) +
-  geom_smooth(method = "lm", fill='grey', color='black', size = 2) +
+  geom_smooth(method="lm", alpha=0, color='black', size=2, fullrange=T) +
   geom_point(aes(shape=grower), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
-  annotate("text", x=15, y=275, size=10, 
-           label=lm_eqn(spread~age, df=young, params=T), parse = T) +
-  annotate("text", x=21, y=350, size=10, 
-           label=lm_eqn(spread~age, df=old, params=T), parse = T) +
-  labs(x="Age", y="Canopy Spread [cm]", 
-       shape = "", title = "C") +
+  ylim(NA, max(age_zero$spread)) +
+  geom_segment(aes(x=16.25, y=375, xend=15.25, yend=475), size=1.25,
+               arrow=arrow(length=unit(0.08, "npc"))) +
+  annotate("text", x=16.25, y=350, size=10, label=lm_intercept(spread~age)) +
+  labs(x="Age", y="Canopy Spread [cm]", shape = "", title = "C") +
   theme_classic(base_size = 24, base_family = "Helvetica") +
   theme(axis.title=element_text(size=36), legend.position="none")
 Z3 <- ggplot(age_zero, aes(x=age, y=height, group=age_class)) +
-  geom_smooth(method = "lm", fill='grey', color='black', size = 2) +
+  geom_smooth(method = "lm", alpha=0, color='black', size = 2, fullrange=T) +
   geom_point(aes(shape=grower), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
-  annotate("text", x=17, y=350, size=10, 
-           label=lm_eqn(height~age, df=young, params=T), parse = T) +
-  annotate("text", x=21, y=425, size=10, 
-           label=lm_eqn(height~age, df=old, params=T), parse = T) +
-  labs(x="Age", y="Height [cm]", 
-       shape = "", title = "B") +
+  ylim(NA, max(age_zero$height)) +
+  geom_segment(aes(x=14.5, y=425, xend=13.5, yend=475), size=1.25,
+               arrow=arrow(length=unit(0.08, "npc"))) +
+  annotate("text", x=15, y=400, size=10, label=lm_intercept(height~age)) +
+  labs(x="Age", y="Height [cm]", shape="", title="B") +
   theme_classic(base_size = 24, base_family = "Helvetica") +
   theme(axis.title=element_text(size=36), legend.position="none")
 Z4 <- ggplot(age_zero, aes(x=age, y=volume, group=age_class)) +
-  geom_smooth(method = "lm", fill='grey', color='black', size = 2) +
+  geom_smooth(method="lm", alpha=0, color='black', size=2, fullrange=T) +
   geom_point(aes(shape=grower), size=10, bg="black") +
   scale_shape_manual(values=c(21:25)) +
-  annotate("text", x=17, y=15, size=10, 
-           label=lm_eqn(volume~age, df=young, params=T), parse = T) +
-  annotate("text", x=21, y=20, size=10, 
-           label=lm_eqn(volume~age, df=old, params=T), parse = T) +
-  labs(x="Age", y="Canopy Volume [m3]", 
-       shape = "", title = "D") +
+  ylim(NA, max(age_zero$volume)) +
+  geom_segment(aes(x=16, y=25, xend=15, yend=40), size=1.25,
+               arrow=arrow(length=unit(0.08, "npc"))) +
+  annotate("text", x=16.5, y=20, size=10, label=lm_intercept(volume~age)) +
+  labs(x="Age", y="Canopy Volume [m3]", shape="", title="D") +
   theme_classic(base_size = 24, base_family = "Helvetica") +
   theme(axis.title=element_text(size=36), legend.position=c(0.7,0.075),
         legend.direction = "horizontal")
