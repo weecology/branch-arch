89212146b0f9808ded9c4f7669502cafaf80899d
Zack Brym <brymz@ufl.edu>
2021-04-02 10:46:06 -0400

    pub-figured.R: lm estimates

--- UtahTarts/pub-figures.R
+++ UtahTarts/pub-figures.R
@@ -24,19 +24,56 @@ per_ha <- expression("Tree Density [ha"^-1*"]")
 ## Figure Generation
 
 # Fig 1
-pdf("Fig1.pdf", width=6, height=5)
+pdf("Fig1.pdf", width=10, height=5)
+A1 <- ggplot(avg_vol, aes(x=age, y=spread/(30.48*spacing_x))) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2),
+              fill='white', color = 'black', size=1.5) +
+  geom_point(aes(shape=grower), size=5, bg="black") +
+  scale_shape_manual(values=c(21:25)) +
+  geom_hline(yintercept=1, linetype=2) +
+  annotate("text", x=25, y=0.6, size=8, 
+           label=lm_eqn(spread/(30.48*spacing_x)~poly(age, 2, raw=T), 
+                        df=avg_vol), parse = TRUE) +
+  geom_text(aes(x=min(age), y=max(spread/(30.48*spacing_x)), label = "A"), 
+            vjust = "inward", hjust = "inward", size = 12) +
+  labs(x="Age", y="In-row Space Filling", shape = "") +
+  theme_classic(base_size = 14, base_family = "Helvetica") +
+  theme(axis.title=element_text(size=20), legend.position="none")
+B1 <- ggplot(avg_vol, aes(x=age, y=(pi*(spread/200)^2*tree_hect/10000))) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2),
+              fill='white', color = 'black', size=1.5) +
+  geom_point(aes(shape=grower), size=5, bg="black") +
+  scale_shape_manual(values=c(21:25)) +
+  annotate("text", x=25, y=0.25, size=8, 
+           label=lm_eqn(((spread/200)^2*tree_hect)~poly(age, 2, raw=T), 
+                        df=age_zero), parse = TRUE) +
+  labs(x="Age", y=ha_ha, shape = "") +
+  geom_text(aes(x=min(age), y=max(pi*(spread/200)^2*tree_hect/10000), 
+                label = "B"), vjust = "inward", hjust = "inward", size = 12) +
+  theme_classic(base_size = 14, base_family = "Helvetica") +
+  theme(axis.title=element_text(size=20), legend.position="none",
+        plot.margin = unit(c(0.3, 0.3, 0.3, 0), "cm"))
+multiplot(A1, B1, cols=2)
+dev.off()
+
+report_lm_poly(spread/(30.48*spacing_x)~poly(age, 2, raw=T), df=avg_vol)
+report_lm_poly(((spread/200)^2*tree_hect)~poly(age, 2, raw=T), df=avg_vol)
+
+
+# Fig 2
+pdf("Fig2.pdf", width=6, height=5)
 ggplot(age_zero, aes(x=age, y=TCSA, group=age_class)) +
-  geom_smooth(method="lm", fill=NA, color='black', size=1.5, fullrange=T) +
+  geom_smooth(method="lm", fill=NA, color='black', size=1.5, fullrange=F) +
   geom_point(aes(shape=grower), size=5, bg="black") +
   scale_shape_manual(values=c(21:25)) +
   ylim(NA, max(age_zero$TCSA)) +
-  geom_segment(aes(x=16, y=175, xend=14, yend=240), size=1,
-               arrow=arrow(length=unit(0.08, "npc"))) +
+  #geom_segment(aes(x=16, y=175, xend=14, yend=240), size=1,
+  #             arrow=arrow(length=unit(0.08, "npc"))) +
   annotate("text", x=12, y=80, size=8, 
            label=lm_eqn(TCSA~age, df=young), parse = T) +
   annotate("text", x=25, y=200, size=8, 
            label=lm_eqn(TCSA~age, df=old), parse = T) +
-  annotate("text", x=16, y=150, size=8, label=lm_intercept(TCSA~age)) +
+  #annotate("text", x=16, y=150, size=8, label=lm_intercept(TCSA~age)) +
   labs(x="Age", y=tcsa_lab, shape = "") +
   theme_classic(base_size=14, base_family="Helvetica") +
   theme(axis.title=element_text(size=20), legend.position="none")
@@ -46,9 +83,30 @@ report_lm(TCSA~age, df=young)
 report_lm(TCSA~age, df=old)
 confint(segmented(lm(TCSA~age, data=avg_vol), seg.Z=~age))
 
+# Fig 3
+pdf("Fig3.pdf", width=6, height=5)
+ggplot(avg_vol, aes(x=TCSA, y=(pi*(spread/200)^2*tree_hect/10000))) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2),
+              fill='white', color = 'black', size=1.5) +
+  geom_point(aes(shape=grower), size=5, bg="black") +
+  scale_shape_manual(values=c(21:25)) +
+  annotate("text", x=300, y=0.25, size=8, 
+           label=lm_eqn(((spread/200)^2*tree_hect)~poly(TCSA, 2, raw=T), 
+                        df=avg_vol), parse = TRUE) +
+  labs(x=tcsa_lab, y=ha_ha, shape = "") +
+  theme_classic(base_size = 14, base_family = "Helvetica") +
+  theme(axis.title=element_text(size=20), legend.position="none",
+        plot.margin = unit(c(0.3, 0.3, 0, 0), "cm"))
+dev.off()
+
+report_lm_poly(((spread/200)^2*tree_hect)~poly(TCSA, 2, raw=T), df=avg_vol)
+
+
+report_lm_poly(((spread/200)^2*tree_hect)~poly(age, 2, raw=T), df=avg_vol)
+report_lm(((spread/200)^2*tree_hect)~age, df=prime)
 
 # Fig 2
-pdf("Fig2.pdf", width=10, height=5)
+pdf("Fig2-extra.pdf", width=10, height=5)
 A1 <- ggplot(age_zero, aes(x=age, y=height/100, group=age_class)) +
   geom_smooth(method = "lm", fill=NA, color='black', size=1.5, fullrange=T) +
   geom_point(aes(shape=grower), size=5, bg="black") +
@@ -84,56 +142,6 @@ report_lm(height~age, df=young)
 report_lm(height~age, df=old)
 confint(segmented(lm(height~age, data=avg_vol), seg.Z=~age))
 report_lm(height~TCSA)
-
-# Fig 3
-pdf("Fig3.pdf", width=6, height=5)
-ggplot(avg_vol, aes(x=age, y=spread/(30.48*spacing_x))) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2),
-              fill='white', color = 'black', size=1.5) +
-  geom_point(aes(shape=grower), size=5, bg="black") +
-  scale_shape_manual(values=c(21:25)) +
-  geom_hline(yintercept=1, linetype=2) +
-  annotate("text", x=25, y=0.6, size=8, 
-           label=lm_eqn(spread/(30.48*spacing_x)~poly(age, 2, raw=T), 
-                        df=age_zero), parse = TRUE) +
-  labs(x="Age", y="In-row Space Filling", shape = "") +
-  theme_classic(base_size = 14, base_family = "Helvetica") +
-  theme(axis.title=element_text(size=20), legend.position="none")
-dev.off()
-
-report_lm_poly(spread/(30.48*spacing_x)~poly(age, 2, raw=T))
-
-# Fig 4
-pdf("Fig4.pdf", width=10, height=5)
-E3 <- ggplot(prime, aes(x=age, y=(pi*(spread/200)^2*tree_hect/10000))) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2),
-              fill='white', color = 'black', size=1.5) +
-  geom_point(aes(shape=grower), size=5, bg="black") +
-  scale_shape_manual(values=c(21:25)) +
-  annotate("text", x=17, y=0.25, size=8, 
-           label=lm_eqn(((spread/200)^2*tree_hect)~age, 
-                        df=prime), parse = TRUE) +
-  labs(x="Age", y=ha_ha, shape = "", title = "A") +
-  theme_classic(base_size = 14, base_family = "Helvetica") +
-  theme(axis.title=element_text(size=20), legend.position="none",
-        plot.margin = unit(c(0.3, 0.3, 0.3, 0), "cm"))
-E4 <- ggplot(prime, aes(x=TCSA, y=(pi*(spread/200)^2*tree_hect/10000))) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2),
-              fill='white', color = 'black', size=1.5) +
-  geom_point(aes(shape=grower), size=5, bg="black") +
-  scale_shape_manual(values=c(21:25)) +
-  annotate("text", x=300, y=0.25, size=8, 
-           label=lm_eqn(((spread/200)^2*tree_hect)~poly(TCSA, 2, raw=T), 
-                        df=prime), parse = TRUE) +
-  labs(x=tcsa_lab, y=ha_ha, shape = "", title = "B") +
-  theme_classic(base_size = 14, base_family = "Helvetica") +
-  theme(axis.title=element_text(size=20), legend.position="none",
-        plot.margin = unit(c(0.3, 0.3, 0, 0), "cm"))
-multiplot(E3, E4, cols=2)
-dev.off()
-
-report_lm_poly(((spread/200)^2*tree_hect)~poly(age, 2, raw=T), df=prime)
-report_lm(((spread/200)^2*tree_hect)~age, df=prime)
 
 # Fig 4
 pdf("Fig4-extra.pdf", width=10, height=8)
@@ -190,6 +198,10 @@ dev.off()
 
 ## Additional curve fitting
 
+report_lm(height~age, df=young)
+report_lm(height~age, df=old)
+confint(segmented(lm(height~age, data=avg_vol), seg.Z=~age))
+report_lm(height~TCSA)
 report_lm_poly(height~poly(age, 2, raw=T), df=age_zero)
 report_lm_poly(spread~poly(age, 2, raw=T), df=age_zero)
 report_lm_poly(volume~poly(age, 2, raw=T), df=age_zero)
